159e055bfdbb907d715da6adc412dcd72061f4e6
Antonito07 <singhdeepak141103@gmail.com>
2025-03-28 10:57:11 +0530

    model bnaye and uske bare m notes bnahya

--- src/utils/apierror.js
+++ src/utils/apierror.js
@@ -22,3 +22,10 @@ class ApiError extends Error {
 }
 
 export {ApiError}
+// jab bhi apierror hoga khin bss iss class ko class kr denge with paramters pass krte hue to yeh class uss 
+// paramters ko set kr dega aur woh throw kr dega 
+
+/// The built-in JavaScript Error class contains basic error properties like:
+// 1️⃣ message → A description of the error
+// 2️⃣ stack → The error's stack trace (helps with debugging)
+// 3️⃣ name → The name of the error (Error by default, or a subclass like ApiError)
